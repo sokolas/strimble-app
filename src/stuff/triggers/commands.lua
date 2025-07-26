1c6f6b4c907e99a251e6956d40d1d541c12215e0
sokolas <sunrise.beast@gmail.com>
2025-07-26 17:13:45 +0200

    global cooldown for commands (#42)

--- src/stuff/triggers/commands.lua
+++ src/stuff/triggers/commands.lua
@@ -3,6 +3,7 @@ local logger = Logger.create("commands")
 local dataHelper = require("src/stuff/data_helper")
 local dialogHelper = require("src/gui/dialog_helper")
 local iconsHelper = require("src/gui/icons")
+local wxtimers = require("src/stuff/wxtimers")
 
 local commandWhere = {
     "At the start",
@@ -11,6 +12,10 @@ local commandWhere = {
 }
 
 local triggerIcons = {}
+
+local cooldownsTimer = nil
+local cooldownsCounter = 0
+local globalCooldowns = {}
 
 local triggerIconPaths = {
     {path = "images/icons/twitch.png", name = "twitch_commands"},
@@ -43,6 +48,11 @@ local function createCommandDlg()
                     type = "choice",
                     choices = commandWhere
                 },
+                {
+                    name = "globalCooldown",
+                    label = "Global cooldown (s)",
+                    type = "text"
+                }
             }
         }
     },
@@ -52,6 +62,8 @@ local function createCommandDlg()
             return false, "'Where' should be specified"
         elseif not data.text or data.text == "" then
             return false, "Text can't be empty"
+        elseif data.globalCooldown ~= string.match(data.globalCooldown, "%d+") then
+            return false, "Only digits are allowed for cooldown"
         else
             return true
         end
@@ -67,23 +79,43 @@ local function matchCommand(trigger, context)
     local message = context.message
     -- local lmessage = Lutf8.lower(message)
     -- Log("checking ", v.name, v.data.text, "in", message, v.data.where, commandWhere[v.data.where + 1])
-    logger.log("matching command", trigger, message)
+    -- logger.log("matching command", trigger, message)
+    logger.log("matching command", trigger.name, trigger.dbId, message)
+
     -- local res = { id = trigger.dbId, text = trigger.data.text, name = trigger.data.name, action = trigger.data.action }
+    local matched = false
     if trigger.data.where == 0 then
         if string.startsWith(message, trigger.data.text) then
-            return true
+            matched = true
         end
     elseif trigger.data.where == 1 then
         local found = Lutf8.find(message, trigger.data.text, 1, true)
         if found then
-            return true
+            matched = true
         end
     else
         if message == trigger.data.text then
-            return true
+            matched = true
         end
     end
-    return false
+
+    local hasCooldown = trigger.data.globalCooldown ~= nil and trigger.data.globalCooldown ~= 0
+    if hasCooldown then
+        local lt = globalCooldowns[trigger.dbId]
+        if lt ~= nil then
+            local elapsed = cooldownsCounter - lt
+            if elapsed < trigger.data.globalCooldown then
+                matched = false
+                logger.log("command " .. trigger.name .. " still on cooldown: ", elapsed)
+            end
+        end
+    end
+
+    if matched then
+        globalCooldowns[trigger.dbId] = cooldownsCounter
+    end
+
+    return matched
 end
 
 local function createTwitchCmdsFolder(triggerListCtrl)
@@ -107,11 +139,51 @@ local function createTwitchCmdsFolder(triggerListCtrl)
             name = "Example command",
             text = "!hello",
             where = 0,
-            enabled = true
+            enabled = true,
+            globalCooldown = 0
         },
-        matches = matchCommand
+        matches = matchCommand,
+        preProcess = function(data)
+            if data.globalCooldown == nil then
+                data.globalCooldown = "0"
+            else
+                data.globalCooldown = tostring(data.globalCooldown)
+            end
+            -- logger.log("reading globalCooldown:", data.globalCooldown)
+        end,
+        postProcess = function(result)
+            if result.globalCooldown and result.globalCooldown ~= "" then
+                result.globalCooldown = tonumber(result.globalCooldown)
+            else
+                result.globalCooldown = 0
+            end
+        end,
+        onEnable = function(item, guiItem)
+            logger.log("resetting global cooldown for ", item.name)
+            globalCooldowns[item.dbId] = nil
+            return true
+        end,
+        onDisable = function(item, guiItem)
+            logger.log("resetting global cooldown for ", item.name)
+            globalCooldowns[item.dbId] = nil
+            return true
+        end,
+        onDelete = function(item, guiItem)
+            logger.log("resetting global cooldown for ", item.name)
+            globalCooldowns[item.dbId] = nil
+            return true
+        end,
+        onUpdate = function(item, guiItem, result)
+            logger.log("resetting global cooldown for ", item.name)
+            globalCooldowns[item.dbId] = nil
+            return true
+        end
     }
     return twitchCmds, treeItem
+end
+
+local function init()
+    cooldownsTimer = wxtimers.addTimer(1000, function(event) cooldownsCounter = cooldownsCounter + 1 end, true)
 end
 
 _M.commandsWhere = commandWhere
@@ -121,5 +193,6 @@ _M.createTriggerFolder = function(name, triggerListCtrl, onTrigger)
     return createTwitchCmdsFolder(triggerListCtrl)
 end
 _M.createCommandDlg = createCommandDlg
+_M.init = init
 
 return _M
